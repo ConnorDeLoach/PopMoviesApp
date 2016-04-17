b4df792cb2dd2947fc85b725c1b8106466bb9cde
Connor DeLoach <connordeloach@gmail.com>
2016-04-16 22:22:43 -0400

    Adds fragment_details layout and places TextView and ImageView inside

--- app/src/main/java/comconnordeloachpopmoviesapp/httpsgithub/popmoviesapp/DetailsFragment.java
+++ app/src/main/java/comconnordeloachpopmoviesapp/httpsgithub/popmoviesapp/DetailsFragment.java
@@ -2,11 +2,16 @@ package comconnordeloachpopmoviesapp.httpsgithub.popmoviesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,11 +32,19 @@ public class DetailsFragment extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.activity_details, container, false);
+        View root = inflater.inflate(R.layout.fragment_details, container, false);
 
-        // Attach data to layout
+        // Create toolbar
+        Toolbar toolbar = (Toolbar) root.findViewById(R.id.app_bar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        // Attach movie title
         TextView textView = (TextView) root.findViewById(R.id.details_test_textview);
         textView.setText(getMovieTitle());
+
+        // Attach movie poster
+        ImageView imageView = (ImageView) root.findViewById(R.id.details_image_view);
+        Picasso.with(getActivity()).load(getMoviePoster()).into(imageView);
         return root;
     }
 
@@ -47,5 +60,21 @@ public class DetailsFragment extends android.support.v4.app.Fragment {
             exc.printStackTrace();
         }
         return title;
+    }
+
+    private String getMoviePoster() {
+        // Create JSON movie object from intent
+        final String BASEURL = "http://image.tmdb.org/t/p/w342/";
+        String posterPath = null;
+        try {
+            JSONObject movie = new JSONObject(getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT));
+            // Fetch movie title
+            posterPath = movie.getString("poster_path");
+
+        } catch (JSONException exc) {
+            Log.e(DetailsFragment.class.toString(), exc.getMessage(), exc);
+            exc.printStackTrace();
+        }
+        return BASEURL + posterPath;
     }
 }
