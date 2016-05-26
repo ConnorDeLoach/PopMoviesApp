de69a0647e2bf1ac230452eec25ab18a82ce53c0
Connor DeLoach <connordeloach@gmail.com>
2016-05-25 19:11:38 -0500

    Moves MyAsyncTask from MainFragment to it's own class

--- app/src/main/java/comconnordeloachpopmoviesapp/httpsgithub/popmoviesapp/MainFragment.java
+++ app/src/main/java/comconnordeloachpopmoviesapp/httpsgithub/popmoviesapp/MainFragment.java
@@ -1,8 +1,6 @@
 package comconnordeloachpopmoviesapp.httpsgithub.popmoviesapp;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -23,21 +21,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 /**
  * Created by connor on 4/11/16.
  */
 public class MainFragment extends Fragment {
 
     // MainFragment variables
-    private CustomAdapter mGridAdapter;
-    private String jsonString = null;
+    public static CustomAdapter mGridAdapter;
 
     public MainFragment() {
         setHasOptionsMenu(true);
@@ -58,11 +48,11 @@ public class MainFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        MyAsyncClass fetchPopMoviePosters = new MyAsyncClass();
+                        MyAsyncTask fetchPopMoviePosters = new MyAsyncTask(getActivity());
                         fetchPopMoviePosters.execute("popular");
                         break;
                     case 1:
-                        MyAsyncClass fetchTopMoviePosters = new MyAsyncClass();
+                        MyAsyncTask fetchTopMoviePosters = new MyAsyncTask(getActivity());
                         fetchTopMoviePosters.execute("top_rated");
                         break;
                 }
@@ -74,7 +64,7 @@ public class MainFragment extends Fragment {
             }
         });
 
-        // Create CustomAdapter connected to singlegrid imageview layout and moviePosterPaths data
+        // Create CustomAdapter connected to single grid imageview layout and moviePosterPaths data
         mGridAdapter = new CustomAdapter(getActivity(), R.layout.single_gridview, R.id.image_view);
         GridView gridView = (GridView) root.findViewById(R.id.grid_view);
 
@@ -134,141 +124,8 @@ public class MainFragment extends Fragment {
         final String RESULTS = "results";
 
         // Construct JSON object and extract movie JSON object (as String)
-        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONObject jsonObject = new JSONObject(MyAsyncTask.jsonString);
         JSONArray moviesArray = jsonObject.getJSONArray(RESULTS);
         return moviesArray.getJSONObject(position).toString();
-    }
-
-    /**
-     * Method that takes the JSON String from AsyncTask and extracts movie poster paths
-     *
-     * @param jsonString from MyAsyncTask
-     * @throws JSONException
-     */
-    private void getImageDataFromJson(String jsonString)
-            throws JSONException {
-        // Names of JSON objects and arrays
-        final String RESULTS = "results";
-        final String MOVIEPOSTER = "poster_path";
-
-        // Construct JSON object and extract movies array
-        JSONObject jsonObject = new JSONObject(jsonString);
-        JSONArray moviesArray = jsonObject.getJSONArray(RESULTS);
-
-        // Clear mGridAdapter
-        mGridAdapter.clear();
-
-        // Go through every index of moviesArray and extract posterPath
-        for (int i = 0; i < moviesArray.length(); i++) {
-            JSONObject movie = moviesArray.getJSONObject(i);
-            // Add posterpath to mGridAdapter
-            mGridAdapter.add(movie.getString(MOVIEPOSTER));
-        }
-    }
-
-    class MyAsyncClass extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            final String TOP_RATED_BASEURL = "http://api.themoviedb.org/3/movie/top_rated";
-            final String POPULAR_MOVIE_BASEURL = "http://api.themoviedb.org/3/movie/popular";
-            final String APPID_PARAM = "api_key";
-
-            //Assign MyAsyncTask params to top_rated URL or popular URL
-            switch (params[0]) {
-                case "popular":
-                    params[0] = POPULAR_MOVIE_BASEURL;
-                    break;
-                case "top_rated":
-                    params[0] = TOP_RATED_BASEURL;
-                    break;
-                default:
-                    Log.e(MyAsyncClass.class.toString(), "Invalid params");
-                    return null;
-            }
-
-            Uri builtUri = Uri.parse(params[0]).buildUpon()
-                    .appendQueryParameter(APPID_PARAM, "YOUR_API_KEY")
-                    .build();
-
-            // Variable declarations outside try block in order to close in Final block
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            try {
-                // Create a URL from the built Uri string above and open a connection to MoviesDB
-                URL url = new URL(builtUri.toString());
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.connect();
-
-                // Setup the urlConnection to be read into a buffered reader
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                // Read the JSON from MoviesDB
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Format JSON String for debugging purposes
-                    buffer.append(line + "\n");
-                }
-                // Get JSON out of try block
-                jsonString = buffer.toString();
-
-            } catch (java.io.IOException exc) {
-                Log.e(MyAsyncClass.class.toString(), exc.getMessage(), exc);
-                exc.printStackTrace();
-                // If error break out of method
-                return null;
-            } finally {
-                // Cleanup open streams
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException exc) {
-                        Log.e(MyAsyncClass.class.toString(), exc.getMessage(), exc);
-                    }
-                }
-            }
-
-            return jsonString;
-        }
-
-        @Override
-        protected void onPostExecute(String jsonString) {
-            // Catch null returns
-            if(jsonString == null) {
-                Log.e(MyAsyncClass.class.toString(), "AsyncTask failed to retrieve data");
-                return;
-            }
-            // Execute getImageDataFromJson task
-            try {
-                getImageDataFromJson(jsonString);
-                // Notify mGridAdapter to refresh
-                mGridAdapter.notifyDataSetChanged();
-            } catch (JSONException exc) {
-                Log.e(MyAsyncClass.class.toString(), exc.getMessage(), exc);
-            }
-            // Insert Data into SQLite DB
-            try {
-                // Initialize SQLite database
-                DBAdapter dbAdapter = new DBAdapter(getActivity());
-
-                // Construct JSON object and extract movies array
-                JSONObject jsonObject = new JSONObject(jsonString);
-                JSONArray moviesArray = jsonObject.getJSONArray("results");
-
-                // Iterate through moviesArray and store the movies' id into database
-                for (int i = 0; i < moviesArray.length(); i++) {
-                    JSONObject movie = moviesArray.getJSONObject(i);
-                    dbAdapter.insertData(DBContract.UID, movie.getString("id"));
-                }
-            } catch (JSONException exc) {
-                Log.e(MyAsyncClass.class.toString(), "Failed to insert data into database");
-            }
-        }
     }
 }
---
+++ app/src/main/java/comconnordeloachpopmoviesapp/httpsgithub/popmoviesapp/MyAsyncTask.java
@@ -0,0 +1,164 @@
+package comconnordeloachpopmoviesapp.httpsgithub.popmoviesapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Connor on 5/25/2016.
+ */
+public class MyAsyncTask extends AsyncTask<String, Void, String> {
+
+    public static String jsonString = null;
+    private Context context;
+
+    MyAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        final String TOP_RATED_BASEURL = "http://api.themoviedb.org/3/movie/top_rated";
+        final String POPULAR_MOVIE_BASEURL = "http://api.themoviedb.org/3/movie/popular";
+        final String APPID_PARAM = "api_key";
+
+        //Assign MyAsyncTask params to top_rated URL or popular URL
+        switch (params[0]) {
+            case "popular":
+                params[0] = POPULAR_MOVIE_BASEURL;
+                break;
+            case "top_rated":
+                params[0] = TOP_RATED_BASEURL;
+                break;
+            default:
+                Log.e(MyAsyncTask.class.toString(), "Invalid params");
+                return null;
+        }
+
+        Uri builtUri = Uri.parse(params[0]).buildUpon()
+                .appendQueryParameter(APPID_PARAM, "YOUR_API_KEY")
+                .build();
+
+        // Variable declarations outside try block in order to close in Final block
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        try {
+            // Create a URL from the built Uri string above and open a connection to MoviesDB
+            URL url = new URL(builtUri.toString());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+
+            // Setup the urlConnection to be read into a buffered reader
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Read the JSON from MoviesDB
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Format JSON String for debugging purposes
+                buffer.append(line + "\n");
+            }
+            // Get JSON out of try block
+            jsonString = buffer.toString();
+
+        } catch (java.io.IOException exc) {
+            Log.e(MyAsyncTask.class.toString(), exc.getMessage(), exc);
+            exc.printStackTrace();
+            // If error break out of method
+            return null;
+        } finally {
+            // Cleanup open streams
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException exc) {
+                    Log.e(MyAsyncTask.class.toString(), exc.getMessage(), exc);
+                }
+            }
+        }
+
+        return jsonString;
+    }
+
+    @Override
+    protected void onPostExecute(String jsonString) {
+        // Catch null returns
+        if (jsonString == null) {
+            Log.e(MyAsyncTask.class.toString(), "AsyncTask failed to retrieve data");
+            return;
+        }
+        // Execute getImageDataFromJson task
+        try {
+            getImageDataFromJson(jsonString);
+            // Notify mGridAdapter to refresh
+            MainFragment.mGridAdapter.notifyDataSetChanged();
+        } catch (JSONException exc) {
+            Log.e(MyAsyncTask.class.toString(), exc.getMessage(), exc);
+        }
+        // Insert Data into SQLite DB
+        try {
+            // Initialize SQLite database
+            DBAdapter dbAdapter = new DBAdapter(context);
+
+            // Construct JSON object and extract movies array
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray moviesArray = jsonObject.getJSONArray("results");
+
+            // Iterate through moviesArray and store the movies' id into database
+            for (int i = 0; i < moviesArray.length(); i++) {
+                JSONObject movie = moviesArray.getJSONObject(i);
+                // Check to see if movie exists in database
+                if (dbAdapter.queryData(movie.getString("id"), new String[]{"_id"}).getCount() == 0)
+                    dbAdapter.insertData(DBContract.UID, movie.getString("id"));
+            }
+        } catch (JSONException exc) {
+            Log.e(MyAsyncTask.class.toString(), "Failed to insert data into database");
+        }
+    }
+
+    /**
+     * Method that takes the JSON String from AsyncTask and extracts movie poster paths
+     *
+     * @param jsonString from MyAsyncTask
+     * @throws JSONException
+     */
+    private void getImageDataFromJson(String jsonString)
+            throws JSONException {
+        // Names of JSON objects and arrays
+        final String RESULTS = "results";
+        final String MOVIEPOSTER = "poster_path";
+
+        // Construct JSON object and extract movies array
+        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray moviesArray = jsonObject.getJSONArray(RESULTS);
+
+        // Clear mGridAdapter
+        MainFragment.mGridAdapter.clear();
+
+        // Go through every index of moviesArray and extract posterPath
+        for (int i = 0; i < moviesArray.length(); i++) {
+            // Retrieve movie JSON object
+            JSONObject movie = moviesArray.getJSONObject(i);
+
+            // Add posterpath to mGridAdapter
+            MainFragment.mGridAdapter.add(movie.getString(MOVIEPOSTER));
+        }
+    }
+}
